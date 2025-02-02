65c10aad0c55cd0968f5a763f5e74289748f795f
Blue Pilkinton-Ching <bluepilkintonching@gmail.com>
2025-02-02 18:19:31 +1300

    0.2.1 - fix express base URL env variable to point to the right server

--- kangaroo.config.ts
+++ kangaroo.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from './src/main/defineConfig';
 export default defineConfig({
   appId: 'org.visvere',
   productName: 'Visvere',
-  version: '0.2.0',
+  version: '0.2.1',
   macOSCodeSigning: false,
   windowsEVCodeSigning: false,
   fallbackToIndexHtml: true,
