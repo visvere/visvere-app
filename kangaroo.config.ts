4e9a26dc9f3658582dfc46998e27cdfb2669274f
Blue <bluepilkintonching@gmail.com>
2025-02-16 22:03:09 +1300

    change version tag

--- kangaroo.config.ts
+++ kangaroo.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from './src/main/defineConfig';
 export default defineConfig({
   appId: 'org.visvere',
   productName: 'Visvere',
-  version: '0.4.0',
+  version: '0.4.1',
   macOSCodeSigning: false,
   windowsEVCodeSigning: false,
   fallbackToIndexHtml: true,
