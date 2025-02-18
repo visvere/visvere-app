896e125d4e33ab57a19eb17bb2da93320bff4a61
Blue <bluepilkintonching@gmail.com>
2025-02-18 23:10:50 +1300

    v0.5.2

--- kangaroo.config.ts
+++ kangaroo.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from './src/main/defineConfig';
 export default defineConfig({
   appId: 'org.visvere',
   productName: 'Visvere',
-  version: '0.5.1',
+  version: '0.5.2',
   macOSCodeSigning: false,
   windowsEVCodeSigning: false,
   fallbackToIndexHtml: true,
